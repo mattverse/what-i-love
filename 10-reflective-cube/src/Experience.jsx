f19f6d41563e3abda614ca8d42a15bcf86b4af60
mattverse <mattpark1028@gmail.com>
2024-02-28 01:55:57 +0900

    10-add presentation control example

--- 10-reflective-cube/src/Experience.jsx
+++ 10-reflective-cube/src/Experience.jsx
@@ -1,38 +1,31 @@
 
-import Lights from './Lights.jsx'
-import { Canvas, useThree, useLoader } from '@react-three/fiber';
-import { useEffect, Suspense } from 'react';
-import { useControls } from 'leva'
+import { useLoader, useFrame } from '@react-three/fiber';
+import { useEffect, Suspense, useRef } from 'react';
 import * as THREE from 'three'
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
 
-import { OrbitControls, AccumulativeShadows, RandomizedLight, Environment, Center, MeshTransmissionMaterial, Html, Text, useGLTF, PresentationControls } from '@react-three/drei'
+import { Environment, Center, MeshTransmissionMaterial, Html, Text, useGLTF, PresentationControls } from '@react-three/drei'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
 
 export default function Experience() {
-    const bgTexture = new THREE.TextureLoader().load("gradient.jpeg");
-
-
     return <>
         {/* <OrbitControls makeDefault enablePan={false} enableZoom={false} /> */}
 
         <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/dancing_hall_1k.hdr" background blur={10} />
         <color args={['white']} attach={"background"} />
-        {/* <Environment map={bgTexture} background blur={10} /> */}
 
+        <PresentationControls>
+            <mesh position={[1, 1, 1]}>
+                <boxGeometry />
+                <meshBasicMaterial />
+            </mesh>
+        </PresentationControls>
 
-
-        <Suspense fallback={null}>
-            <PresentationControls
-                azimuth={[-Infinity, Infinity]} // Horizontal limits
-                polar={[-Infinity, Infinity]} // Vertical limits
-
-            >
-                <FBXModel position={[0.05, -0.02, 0]} url="chamfercube.fbx" />
-            </PresentationControls>
-            {/* <FBXModel position={[0, 0, 0]} url="chamfercube.fbx" /> */}
-        </Suspense>
+        {/* <Suspense fallback={null}>
+            <FBXModel position={[2, -0.6, 0]} url="chamfercube.fbx" />
+            <FBXModel position={[-2, 0.6, 0]} url="chamfercube.fbx" />
+        </Suspense> */}
 
         <CustomText position={[-5, 3.5, -5]}>MOMENT</CustomText>
         <CustomText position={[-5, 2, -5]}>OF</CustomText>
@@ -69,37 +62,47 @@ function FBXModel({ url, position }) {
         }
     );
 
+    const cube1 = useRef()
+
+    useFrame((state, delta) => {
+        cube1.current.rotation.y += delta * 0.5
+    })
 
     // This function will be called for each mesh in the FBX model
     const applyMaterial = (child) => {
         return (
             <>
-                <mesh
-                    key={child.id}
-                    geometry={child.geometry}
-                    position={position} // Adjust position as needed
-                    scale={[0.035, 0.035, 0.035]} // Adjust scale as needed
-                >
-                    <MeshTransmissionMaterial
-                        roughness={0}
-                        transmission={1}
-                        thickness={0.8}
-                        iridescence={1}
-                        iridescenceIOR={0.7}
-                        envMap={hdrEquirect}
-                        envMapIntensity={1.5}
-                        // map={envMap}aa
-                        chromaticAberration={0.2}
-                    // background={new THREE.Color(config.bg)}
-                    />
-                </mesh>
+                <Center>
+                    <mesh
+                        ref={cube1}
+                        key={child.id}
+                        geometry={child.geometry}
+                        // position={position} // Adjust position as needed
+                        scale={[0.035, 0.035, 0.035]} // Adjust scale as needed
+                    >
+                        <MeshTransmissionMaterial
+                            roughness={0}
+                            transmission={1}
+                            thickness={0.8}
+                            iridescence={1}
+                            iridescenceIOR={0.7}
+                            envMap={hdrEquirect}
+                            envMapIntensity={1.5}
+                            // map={hdrEquirect}
+                            chromaticAberration={0.2}
+                        />
+                    </mesh>
+                </Center>
+
 
             </>
         );
     };
 
     return (
-        <group scale={30}>
+        <group scale={30}
+            position={position}
+        >
             {fbx.children.map((child) => {
                 if (child.isMesh) {
                     return applyMaterial(child);
