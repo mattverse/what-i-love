43f6b7db65a29815cb678bde74ddd662a3a7e009
mattverse <mattpark1028@gmail.com>
2024-03-05 19:16:07 +0900

    5- Add rotating laptop

---
+++ 5-rotating-laptop/src/index.jsx
@@ -0,0 +1,24 @@
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import { ScrollControls } from '@react-three/drei'
+
+import Experience from './Experience.jsx'
+
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <Canvas
+        camera={{
+            fov: 45,
+            near: 0.1,
+            far: 2000,
+            position: [-3, 1.5, 4]
+        }}
+    >
+        <ScrollControls >
+            <Experience />
+        </ScrollControls>
+    </Canvas>
+)
