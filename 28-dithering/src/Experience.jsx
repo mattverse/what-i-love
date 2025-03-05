2e5055b59c42456fe121fbeeb8609cb79ffefbec
mattverse <mattpark1028@gmail.com>
2025-03-05 00:41:00 -0500

    Rename folders for correct sorting

---
+++ 28-dithering/src/Experience.jsx
@@ -0,0 +1,11 @@
+import { OrbitControls } from '@react-three/drei'
+import Lights from './Lights.jsx'
+
+
+export default function Experience() {
+    return <>
+        <OrbitControls makeDefault />
+        <Lights />
+
+    </>
+}
