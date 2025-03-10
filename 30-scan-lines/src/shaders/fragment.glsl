3dbd15ad736801d65638d2f8e5ad06277c9f0c38
mattverse <mattpark1028@gmail.com>
2025-03-10 14:52:05 -0400

    30 - Add background lines to fragment shader

--- 30-scan-lines/src/shaders/fragment.glsl
+++ 30-scan-lines/src/shaders/fragment.glsl
@@ -6,7 +6,7 @@ void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
     // pointer pos scales from -1 ~ 1 so we need to normalize it to 0~1 range. 
     vec2 normalizedPointerPos = uPointer * 0.5 + 0.5;
     float dist = length(uv - normalizedPointerPos);
-    float radius = 0.15;
+    float radius = 0.2;
 
     float maxZoom = 2.0;
     float zoomFactor = mix(maxZoom, 1.0, smoothstep(0.0, radius, dist));
@@ -23,6 +23,9 @@ void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
 
     vec4 color = texture2D(inputBuffer, uvPixel);
     color.rgb *= border.y;
+
+    float lines = sin(uv.y * 700.0 + time * 100.);
+    color *= lines + 1.8;
 
     // for brightness
     color.rgb *= 10.0;
